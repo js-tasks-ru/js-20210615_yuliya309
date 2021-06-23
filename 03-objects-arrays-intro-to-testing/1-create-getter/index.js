dded4e64fc98ba30d34700a5239c1530388694d8
Юлия Ионова <y_ionova@step.ru>
2021-06-23 20:32:06 +0300

    Выполнена задача - task createGetter.

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,21 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    return function(object)
+    {
+        let arrayKey = path.split(".");
+        let obj = object;
+        for(let key of arrayKey)
+        {
+            if(key in obj)
+            {
+                if(typeof obj[key] === "object")    
+                    obj = obj[key];
+                else    
+                    return obj[key];
+            }
+        }
+        return obj[arrayKey[arrayKey.length - 1]];
 
+    }
 }
