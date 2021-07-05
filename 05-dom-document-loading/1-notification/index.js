f23e1c6f9b602779b71c1faf51d9468655dffa96
y_ionova <y_ionova@step.ru>
2021-07-05 13:10:38 +0300

    Выполнена задача - task NotificationMessage компонент.

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,73 @@
 export default class NotificationMessage {
 
+    static oldElement = null;
+    static oldTime = null;
+    element = null;
+
+    constructor(textValue, {
+        duration = 0,
+        type = ''
+      } = {})
+    {
+        this.textValue = textValue;
+        this.duration = duration;
+        this.type = type;
+
+        this.createElement();
+    }
+
+    createElement()
+    {
+        this.element = document.createElement('div');
+        this.element.style = `--value:${this.duration / 1000}s`;
+        this.element.className = `notification ${this.type}`;
+        this.element.innerHTML = this.getInner();
+
+    }
+    getInner()
+    {
+             return `
+             <div class="timer"></div>
+             <div class="inner-wrapper">
+               <div class="notification-header">${this.type}</div>
+               <div class="notification-body">
+                ${this.textValue}
+               </div>
+             </div>
+            `;
+    }
+
+    show(parent = document.body)
+    {
+        if(NotificationMessage.oldElement !== null)
+        {
+            NotificationMessage.oldElement.remove();
+            clearTimeout(NotificationMessage.oldTime);
+        }
+
+        NotificationMessage.oldElement = this.element;
+        parent.append(this.element);
+
+        NotificationMessage.oldTime = setTimeout(() => {
+            this.remove();
+        }, this.duration);
+
+    }
+
+    remove()
+    {
+        this.element.remove();
+        clearTimeout(NotificationMessage.oldTime);
+    }
+
+    destroy()
+    {
+        if(this.element !== null)
+        {
+            this.remove();
+        }
+        NotificationMessage.oldElement = null;
+        NotificationMessage.oldTime = null;
+        this.element = null;
+    }
 }
